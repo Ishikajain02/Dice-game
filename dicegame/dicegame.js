b3993978382385af5e3c77d83ff140099d0aaf5d
Ishikajain02 <94300478+Ishikajain02@users.noreply.github.com>
2023-01-27 00:20:06 +0530

    Add files via upload

---
+++ dicegame/dicegame.js
@@ -0,0 +1,21 @@
+
+
+var random1 = Math.floor(Math.random()* 6)+1;
+var randomDiceImage ="dice" +random1 +".png"; 
+var randomImageSource = "images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var random2 = Math.floor(Math.random()*6)+1;
+var randomImage2 = "images/dice"+random2 +".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImage2);
+if(random1>random2){
+    document.querySelector("h1").innerHTML="Player 1 wins";
+}
+else if(random2>random1){
+    document.querySelector("h1").innerHTML="Player 2 wins";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw";
+}
